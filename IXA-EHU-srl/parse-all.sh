e57296944aa04eabe29464a10fd686a7b40b7f79
ngocminh.oss <ngocminh.oss@gmail.com>
2014-09-25 15:41:17 +0200

    parse all

---
+++ IXA-EHU-srl/parse-all.sh
@@ -0,0 +1,7 @@
+$DIR=$1
+
+echo "The program will parse `find $DIR -type f | wc -l` files."
+echo "The result of file X will be file X.out in the same directory."
+echo "Started at: `date`"
+find $DIR -type f -exec sh -c "cat '{}' | java -cp IXA-EHU-srl-1.0.jar ixa.srl.SRLClient > '{}.out'" \;
+echo "Finished at: `date`"
