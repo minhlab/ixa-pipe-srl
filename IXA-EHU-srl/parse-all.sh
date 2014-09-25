10a4a1b44f87e083d3c8e9baffb63e0f81cc8d86
ngocminh.oss <ngocminh.oss@gmail.com>
2014-09-25 16:01:15 +0200

    print status

--- IXA-EHU-srl/parse-all.sh
+++ IXA-EHU-srl/parse-all.sh
@@ -3,5 +3,5 @@ $DIR=$1
 echo "The program will parse `find $DIR -type f | wc -l` files."
 echo "The result of file X will be file X.out in the same directory."
 echo "Started at: `date`"
-find $DIR -type f -exec sh -c "cat '{}' | java -cp IXA-EHU-srl-1.0.jar ixa.srl.SRLClient > '{}.out'" \;
+find $DIR -type f -exec sh -c "echo -n '{}... ' && cat '{}' | java -cp IXA-EHU-srl-1.0.jar ixa.srl.SRLClient > '{}.out' && echo 'Done.'" \;
 echo "Finished at: `date`"
